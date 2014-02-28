21b83be75eb3d7dd5f85c09e8f1f8468cc3a90ef
godfried.moudondi <godfried.moudondi@te-laval.fr>
2014-02-28 12:12:34 +0100

    Added: * Grid system (12 and 16 columns) * Useful mixins * Variables and more

--- config.rb
+++ config.rb
@@ -1,5 +1,8 @@
 # Require any additional compass plugins here.
 
+# Increase percentage precision; here 8 numbers after the comma.
+Sass::Script::Number.precision = 8
+
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
@@ -17,7 +20,7 @@ relative_assets = true
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 #line_comments = false
 
- 
+
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
