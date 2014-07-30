0241edfc179ee1e71c4af235bc226532838c6c49
godfried.moudondi <godfried.moudondi@te-laval.fr>
2014-07-30 17:05:09 +0200

    Start project with Grunt Task Runner

---
+++ Gruntfile.js
@@ -0,0 +1,175 @@
+module.exports = function(grunt) {
+
+    /* This module avoid single plugin load for each one added */
+    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+
+        // This makes the package values accessible
+        pkg: grunt.file.readJSON('package.json'),
+
+        //Here we add configuration for our plugins
+
+        // Minify our javaScript
+        uglify: {
+
+            options: {
+              // Add a date to tag when the file was generated.
+              banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+
+            common: {
+
+                files: {
+                    'js/dest.js': ['js/source.js', 'js/source2.js'],
+                    'js/dest.js': ['js/source.js']
+                }
+            }
+        },
+
+        // Compile our javaScript in one file
+        concat: {
+
+            compile: {
+
+                files: {
+                    'js/dest.js': ['js/source.js', 'js/source2.js'],
+                    'js/dest.js': ['js/source.js']
+                }
+            }
+        },
+
+        /** Plugin: OPTIMIZE IMAGES -- Name : imagemin **/
+        imagemin: {
+
+            // Static images optimization
+            /*static: {                          // Target
+              options: {                       // Target options
+                optimizationLevel: 10
+              },
+              files: {                         // Dictionary of files
+                'img/top_espot_bg.jpg': 'img/top_espot_bg.jpg', // 'destination': 'source'
+                'img/logo-footer.jpg': 'img/logo-footer.jpg',
+                'img/product1-full.jpg': 'img/product1-full.jpg'
+              }
+            },*/
+
+            // Dynamic images optimization
+            dynamic: {                         // Another target
+              options: {                       // Target options
+                optimizationLevel: 10
+              },
+              // HTML images
+              files: [
+                  {
+                    expand: true,                  // Enable dynamic expansion
+                    cwd: 'img/',                   // Src matches are relative to this path
+                    src: ['**/*.{png,jpg,jpeg,gif}'],   // Actual patterns to match
+                    dest: 'img/'                  // Destination path prefix
+                  },
+                  // css images
+                  {
+                    expand: true,
+                    cwd: 'css/img',
+                    src: ['**/*.{png,jpg,jpeg}'],
+                    dest: 'css/img'
+                  }
+              ]
+            }
+
+        },
+
+        /** Plugin: NOTOFITY ERRORS -- Name : notify hooks **/
+        notify_hooks: {
+
+          options: {
+            enabled: true,
+            max_jshint_notifications: 5, // maximum number of notifications from jshint output
+            title: "Project Name" // defaults to the name in package.json, or will use project directory's name
+          },
+
+          watch: {
+            options: {
+              title: 'Task Complete',  // optional
+              message: 'Concat and Uglify finished running', //required
+            }
+          }
+
+        },
+
+        /** Plugin: VALIDATE HTML FILES WITH W3C -- Name : validation **/
+        validation: {
+          options: {
+            reset: grunt.option('reset') || false,
+            stoponerror: false,
+            reportpath: false,
+            reset: true,
+            /*remotePath: "http://decodize.com/",
+            remoteFiles: ["html/moving-from-wordpress-to-octopress/","css/site-preloading-methods/"], //or
+            remoteFiles: "validation-files.json", // JSON file contains array of page paths. */
+            relaxerror: ["Bad value X-UA-Compatible for attribute http-equiv on element meta."] //ignores these errors
+          },
+
+          files: {
+            src: ['*.html']
+          }
+        },
+
+        uncss: {
+          options: {
+            ignore: ['#added_at_runtime', /test\-[0-9]+/],
+            media: ['(min-width: 768px)'],
+            report: false,
+            timeout: 1000
+          },
+
+          dist: {
+            files: {
+              'css/cssfile.css': ['HTML-file.html'] // Add each css file for corresponding html file here
+              }
+            }
+        },
+
+        /** Plugin: WATCH AND RUN CONFIGURE TASKS -- Name : watch **/
+        watch: {
+
+          /**  One Config per task to avoid unfound task bug **/
+          // concat before compression.
+          concat: {
+
+            files: ['js/*.js'],
+            tasks: ['concat'],
+
+            options: {
+              spawn: false,
+            },
+
+          },
+
+          // compression
+          uglify: {
+
+            files: ['js/*.js'],
+            tasks: ['uglify'],
+
+            options: {
+              spawn: false,
+            },
+
+          }
+
+        }
+
+
+    });
+
+
+    // Default task launched when 'grunt' command is typed on terminal
+    grunt.registerTask('default', ['watch']);
+
+    grunt.registerTask('server', [
+    'watch',
+    'server',
+    'notify:server'
+    ]);
+};
---
+++ README-Grunt.txt
@@ -0,0 +1,56 @@
+
+If this is the first time you hear about #GRUNT make sure you read an introduction here: http://gruntjs.com/
+
+Here a short resume; Grunt is javascript task runner ( a NodeJs module ); NodeJs use npm to handle its module so before you try to install
+#GRUNT you have to install NodeJS first.
+
+Here is the official website: http://nodejs.org/
+Grab the source there and make a manuel install (safer as much i know) : https://github.com/joyent/node/wiki/installation
+
+Here is the NodeJs install procedure: (Excerpt from @joyent github wiki)
+
+1. Reach the directory where the downloaded source of NodeJS is
+2. tar -zxf node-v0.6.18.tar.gz #Download this from nodejs.org
+3. cd cd node-v0.6.18 #to the extracted folder
+4. ./configure && make && sudo make install
+5. Cross fingers and wait
+6. Type the command node -v to check your NodeJS version and make sure everything is fine.
+
+NOTE: Depending on your OS you can through some trouble, if so check the link above (joyent's wiki link)
+
+Now the NodeJs is installed you can start a project:
+
+1. Type npm init in your project directory (where you want to start, no matter) and fill all the fields.
+2. Your file should name package.js and contains all the project's description (editable for later).
+
+
+Now you can install #GRUNT (tip: type npm -v to make sure you are good to go)
+
+NOTE: add 'sudo' before you have a permission denied message. (Unix)
+
+1. npm install -g grunt-cli (install grunt CLI)
+2. npm install grunt --save-dev (install grunt for your project)
+3. now you can install grunt tasks (a list above and more if they suit your project)
+
+NOTE: make sure your #GRUNT version is supported for each task before you install them.
+
+- Grunt Matchdep (globally add all existing module with a single line) sudo npm install matchdep
+- Grunt Uglify (compress js files) : sudo npm install grunt-contrib-uglify --save-dev
+- Grunt Concat (concat js files) : sudo npm install grunt-contrib-concat --save-dev
+- Grunt Concat (optimize images, check other alternatives) : sudo npm install grunt-contrib-imagemin --save-dev
+- Grunt Watch (watch and run some tasks when some files are edited) : sudo npm install grunt-contrib-watch --save-dev
+- Grunt Notify (Notify error on the screen, useful) : sudo npm install grunt-notify --save-dev
+- Grunt HTML Validation (locally validate all your html files) : sudo npm install grunt-notify --save-dev
+- Grunt UnCSS (remove unsed css properties for a page) : sudo npm install grunt-uncss --save-dev
+- Grunt SVGMin (minify svg) : sudo npm install grunt-svgmin --save-dev
+
+NOTE: Refer to each task page on Github for more infos on settings.
+
+Last but the least, the Gruntfile.js
+
+This is grunt configurtion file and it contains its settings (of course).
+
+You can find one in this project and edit it if you add another tasks.
+
+
+Done. Now Dance.
