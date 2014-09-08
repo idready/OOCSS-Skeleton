e4ca60139ce20d467d28d6cd7699528712384568
idready <you_sombo@hotmail.fr>
2014-09-08 19:06:02 +0000

    IE10 on W8 and W8 Phone bugfix for viewport

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 
+// Viewport bugfix for IE10 on Windows 8 & W8 Phone (< update 3)
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+  var msViewportStyle = document.createElement('style')
+  msViewportStyle.appendChild(
+    document.createTextNode(
+      '@-ms-viewport{width:auto!important}'
+    )
+  )
+  document.querySelector('head').appendChild(msViewportStyle)
+}
+
+
+
 jQuery(document).ready( function() {
 
     // check if element exists
